c7605bd8d749b0c47876faa7103d0004145d9d7f
Raul Quispe <relaxedchild@gmail.com>
2021-07-03 12:38:10 -0500

    Updates

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ import validator from "./validator.js";
 
 const creditCardElement = document.getElementById("credit-card-input");
 const maskedCreditCardElement = document.getElementById("masked-credit-card");
+const validationResultElement = document.getElementById("validation-result");
 
 creditCardElement.addEventListener("keyup", function () {
   const cardNumber = creditCardElement.value;
-  console.log(validator.isValid(cardNumber));
+
+  if (validator.isValid(cardNumber)) {
+    validationResultElement.innerHTML = "Credit card is valid";
+    validationResultElement.style.color = "green";
+  } else {
+    validationResultElement.innerHTML = "credit card is invalid";
+    validationResultElement.style.color = "red";
+  }
+
   maskedCreditCardElement.innerHTML = validator.maskify(cardNumber);
 });
