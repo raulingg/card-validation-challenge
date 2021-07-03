f4521b6fe91012e61e0b630fa7b73f2112bb0cb2
Raul Quispe <relaxedchild@gmail.com>
2021-07-03 12:30:20 -0500

    implement isValid and maskify functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const creditCardElement = document.getElementById("credit-card-input");
+const maskedCreditCardElement = document.getElementById("masked-credit-card");
+
+creditCardElement.addEventListener("keyup", function () {
+  const cardNumber = creditCardElement.value;
+  console.log(validator.isValid(cardNumber));
+  maskedCreditCardElement.innerHTML = validator.maskify(cardNumber);
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,37 @@
 const validator = {
-  // ...
+  isValid(cardNumber) {
+    const numbers = Array.from(cardNumber).map(Number);
+    console.log(numbers);
+
+    // Step 2: double all digits which are in even positions
+    const doubledNumbers = numbers.map((digit, idx) =>
+      idx % 2 == 0 ? digit * 2 : digit
+    );
+    console.log(doubledNumbers);
+
+    // Step 3: sum the digits of all numbers, from previous step, greater than 9
+    const nextNumbers = doubledNumbers.map((number) =>
+      number > 9 ? 1 + (number % 10) : number
+    );
+    console.log(nextNumbers);
+
+    // Step 4: Sum all the final digits
+    const sum = nextNumbers.reduce((sum, number) => number + sum, 0);
+
+    console.log(sum);
+    // Step 5: Check if the last digit of the sum is 0
+    return sum % 10 == 0;
+  },
+  maskify(cardNumber, maskCharacter = "#") {
+    if (cardNumber.length > 4) {
+      return (
+        maskCharacter.repeat(cardNumber.length - 4) +
+        cardNumber.slice(cardNumber.length - 4, cardNumber.length)
+      );
+    }
+
+    return cardNumber;
+  },
 };
 
 export default validator;
