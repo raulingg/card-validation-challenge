e481aafe249ed0ff55895b811ee65a0eb2e60a3c
Raul Quispe <relaxedchild@gmail.com>
2021-07-05 17:46:18 -0500

    Updates

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import validator from "./validator.js";
 const creditCardElement = document.getElementById("credit-card-input");
 const maskedCreditCardElement = document.getElementById("masked-credit-card");
 const validationResultElement = document.getElementById("validation-result");
-
+const containerElement = document.getElementById("creditCardContainer");
+let inFront = true;
 creditCardElement.addEventListener("keyup", function () {
   const cardNumber = creditCardElement.value;
 
@@ -16,4 +17,19 @@ creditCardElement.addEventListener("keyup", function () {
   }
 
   maskedCreditCardElement.innerHTML = validator.maskify(cardNumber);
+});
+
+containerElement.addEventListener("click", function () {
+  const creditCardFrontElement = document.getElementById("creditCardFront");
+  const creditCardBackElement = document.getElementById("creditCardBack");
+
+  if (inFront) {
+    creditCardFrontElement.style.transform = "rotateY(180deg)";
+    creditCardBackElement.style.transform = "rotateY(0deg)";
+    inFront = false;
+  } else {
+    creditCardFrontElement.style.transform = "rotateY(0deg)";
+    creditCardBackElement.style.transform = "rotateY(180deg)";
+    inFront = true;
+  }
 });
