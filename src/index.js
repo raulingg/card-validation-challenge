8c07aac65c389d90c12a39dab54a4db5d5c1e0ff
Raul Quispe <relaxedchild@gmail.com>
2021-08-08 20:23:33 -0500

    add form validation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import validator from "./validator.js";
 
-const formElement = document.querySelector(".form");
+const formElement = document.querySelector("form");
 const cvvInput = document.getElementById("cvv");
 const maskedCreditCardElement = document.getElementById("masked-credit-card");
-const validationResultElement = document.getElementById("validation-result");
-const cardIssuerIconElement = document.querySelector(".cardIssuerIcon");
+const cardIssuerIconElements = document.querySelectorAll(".cardIssuerIcon");
+const cardNumberElement = document.getElementById("number");
 
 let creditCardInFront = true;
 
@@ -29,43 +29,66 @@ const behaviorsFns = {
   },
   validAndMaskify(e) {
     const cardNumber = e.target.value;
-
-    if (validator.isValid(cardNumber)) {
-      validationResultElement.innerHTML = "Credit card is valid";
-      validationResultElement.style.color = "green";
-    } else {
-      validationResultElement.innerHTML = "credit card is invalid";
-      validationResultElement.style.color = "red";
-    }
     maskedCreditCardElement.innerHTML = validator.maskify(cardNumber);
-  },
-  showCardIssuerIcon(e) {
-    const issuer = validator.getIssuer(e.target.value);
 
-    if (issuer) {
-      cardIssuerIconElement.className = `cardIssuerIcon ${issuer}Icon`;
+    const issuer = validator.getIssuer(cardNumber);
+    const valid = validator.isValid(cardNumber);
+
+    if (!valid || !issuer) {
+      cardNumberElement.className = "invalid";
+      cardIssuerIconElements.forEach((el) => (el.innerHTML = ""));
+      return;
+    }
+
+    if (valid && issuer) {
+      cardNumberElement.className = "valid";
+      const imageTemplate = `<img data-issuer="${issuer}" src="./assets/issuers/${issuer}.svg" />`;
+      cardIssuerIconElements.forEach((el) => {
+        if (el.dataset.issuer != issuer) {
+          el.innerHTML = imageTemplate;
+        }
+      });
+      document.getElementById("month").focus();
     }
   },
 };
 
-formElement.addEventListener("keyup", function (e) {
+formElement.addEventListener("focusout", function (e) {
+  e.stopPropagation();
+
+  if (e.target.id == "number") return;
+
+  const element =
+    e.target.id == "month" || e.target.id == "year"
+      ? document.querySelector(".expirationInput")
+      : e.target;
+
+  if (!e.target.checkValidity()) {
+    element.classList.add("invalid");
+    element.classList.remove("valid");
+  } else {
+    element.classList.add("valid");
+    element.classList.remove("invalid");
+  }
+});
+
+formElement.addEventListener("input", function (e) {
   const { behavior } = e.target.dataset;
 
-  if (e.target.id === "number") {
-    behaviorsFns.showCardIssuerIcon(e);
+  if (e.target.id != "name" && isNaN(Number(e.target.value))) {
+    e.target.value = e.target.value.replace(/[^\d]+/, "");
+    return;
   }
 
-  if (
-    "focusSibling" === behavior &&
-    e.target.value.length === e.target.maxLength
-  ) {
+  if (!e.target.checkValidity()) {
+    return;
+  }
+
+  if ("focusSibling" === behavior) {
     behaviorsFns.focusSibling(e);
   }
 
-  if (
-    "validAndMaskify" === behavior &&
-    e.target.value.length >= e.target.minLength
-  ) {
+  if ("validAndMaskify" === behavior) {
     behaviorsFns.validAndMaskify(e);
   }
 });
